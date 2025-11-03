49339479527fb7e5f5d6b79c7ee42abfc7b436ea
Adnan El Assadi <aassadi22@ku.edu.tr>
2025-11-03 15:06:53 -0500

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h2 className="text-4xl text-brand">
+        Store It - The only storage solution you need
+      </h2>
+    </div>
+  );
+}
